d6f85096d5625df99e49afca0ec545a98b82e4db
mikesmithjr <mikesmithjr@fullsail.edu>
2013-09-09 20:01:03 -0500

    Setting up for the Week and getting the network status functioning

---
+++ Week 2/WhenWhereHow/gen/com/wickedsoftwaredesign/whenwherehow/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wickedsoftwaredesign.whenwherehow;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
