62cbb8df8c976d78ef891f1d0e6e474a6bbe89cd
mikesmithjr <mikesmithjr@fullsail.edu>
2013-09-11 23:57:24 -0500

    Finishing some commenting and building APK

--- Week 2/WhenWhereHow/src/com/wickedsoftwaredesign/whenwherehow/MoviePlayer.java
+++ Week 2/WhenWhereHow/src/com/wickedsoftwaredesign/whenwherehow/MoviePlayer.java
@@ -1,3 +1,12 @@
+/*
+ * project     WhenWhereHow
+ * 
+ * package		com.wickedsoftwaredesign.whenwherehow
+ * 
+ * @author     Michael R. Smith Jr
+ * 
+ * date			Sep 11, 2013
+ */
 package com.wickedsoftwaredesign.whenwherehow;
 
 
@@ -12,12 +21,19 @@ import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MoviePlayer.
+ */
 public class MoviePlayer extends Activity implements OnClickListener{
 
 	
 	VideoView viewer;
 	Button vidButton;
 	
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,11 +42,15 @@ public class MoviePlayer extends Activity implements OnClickListener{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.activity_movie_player);
-		
+		//building the button from the xml file
 		vidButton = (Button) findViewById(R.id.playVideoButton);
+		//setting the on click listener
 		vidButton.setOnClickListener(this);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -38,13 +58,21 @@ public class MoviePlayer extends Activity implements OnClickListener{
 		return true;
 	}
 
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		//when the button is clicked the video view is built
 		viewer = (VideoView) findViewById(R.id.videoPlayer);
+		//the file path string is created
 		String path = "android.resource://" + getPackageName() + "/" + R.raw.mdf3video;
+		//the path uri is parsed and passed to the video viewer
 		viewer.setVideoURI(Uri.parse(path));
+		//player control buttons are enabled
 		viewer.setMediaController(new MediaController(this));
+		//video is started
 		viewer.start();
 	}
 
