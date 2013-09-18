dfb3e0044da9decabfd16bd49024ae741a09577a
mikesmithjr <mikesmithjr@fullsail.edu>
2013-09-18 00:56:42 -0500

    Action Bar and overflow working now working on the search or navigation functions

--- Week 3/WhenWhereHowActionBar/src/com/wickedsoftwaredesign/whenwherehowactionbar/MainActivity.java
+++ Week 3/WhenWhereHowActionBar/src/com/wickedsoftwaredesign/whenwherehowactionbar/MainActivity.java
@@ -145,6 +145,7 @@ public class MainActivity extends Activity implements OnClickListener, LocationL
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
 		return true;
 	}
 	
